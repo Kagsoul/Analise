0105e7c9e002a28ea60d86bf68952ab64e69c0a2
Kagsoul <160343652+Kagsoul@users.noreply.github.com>
2025-03-31 18:17:45 -0300

    Add files via upload

---
+++ estrutura_senha.py
@@ -0,0 +1,29 @@
+import sys
+import hashlib
+cadastro = input ("enter")
+pausa = 0
+while True:
+    for cadastro in range (1):
+        nome = input ("Digite seu nome: ")
+        senha = input ("digite sua senha: ")
+        senha = ("")
+        gmail = input ("digite seu Email: ")
+        gmail = ("")
+
+        print (f"seu nome de usuario e {nome}, e o seu email {gmail}")
+    pausa = input ("enter para confirmar todas as informaões")
+    break
+
+while hashlib:
+    for erro in range (1):
+     input ("sua senha ou email estão errado, verifique: ")
+     print (f"senha {senha}| Gmail {gmail}")
+     input ("vamos conferir aperte <enter> para continua")
+     senha = input ("digite sua senha novamente")
+     gmail = input ("digite seu email novamente")
+     break
+
+    def hashlib (senha, gmail,):
+     hashlib.algorithms_available (senha, gmail) 
+
+     hashlib.algorithms_guaranteed (senha, gmail)
